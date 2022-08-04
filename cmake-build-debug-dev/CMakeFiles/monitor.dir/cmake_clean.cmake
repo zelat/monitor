0f40a1bfbd3d2075f1e9d9348e9bb33ae4f26527
tanchao <zmntc168@163.com>
2022-08-04 19:54:55 +0800

    Initial commit

---
+++ cmake-build-debug-dev/CMakeFiles/monitor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/monitor.dir/main.c.o"
+  "monitor"
+  "monitor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/monitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
